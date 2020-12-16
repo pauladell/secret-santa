f97f402ad78e3a84df887e14cc0bff40a795744a
Paloma Auladell <auladell.leon@gmail.com>
2020-12-16 21:30:59 +0100

    last mod

--- secret-santa/src/app/secret-santa-board/secret-santa-board.component.ts
+++ secret-santa/src/app/secret-santa-board/secret-santa-board.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+type SecretSantaItemObj = {
+  name_displayed: string;
+  name_hidden: String;
+};
 @Component({
   selector: 'app-secret-santa-board',
   templateUrl: './secret-santa-board.component.html',
-  styleUrls: ['./secret-santa-board.component.scss']
+  styleUrls: ['./secret-santa-board.component.scss'],
 })
 export class SecretSantaBoardComponent implements OnInit {
+  @Input() friendsList: string[] = ['luis', 'paloma'];
+  secretSantaList: SecretSantaItemObj[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.buildSecretSanta();
   }
-
+  buildSecretSanta() {
+    console.log('entra en builsecretsanta');
+    this.secretSantaList = this.friendsList.map((item) => {
+      let tupla: SecretSantaItemObj = {
+        name_displayed: item,
+        name_hidden: item,
+      };
+      console.log(tupla);
+      return tupla;
+    });
+  }
+  ngOnInit(): //objeto.buildSecretSanta();
+  void {}
 }
--- secret-santa/src/app/secret-santa-card/secret-santa-card.component.ts
+++ secret-santa/src/app/secret-santa-card/secret-santa-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-secret-santa-card',
   templateUrl: './secret-santa-card.component.html',
-  styleUrls: ['./secret-santa-card.component.scss']
+  styleUrls: ['./secret-santa-card.component.scss'],
 })
 export class SecretSantaCardComponent implements OnInit {
+  @Input() name_displayed: string;
+  @Input() name_hidden: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
